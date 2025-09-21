d7934d3031039675dff7da1fe57dd5f137b98801
Gon <o.gonzalo@hotmail.com>
2025-09-21 15:11:08 -0300

    fix: typo

--- compiler/date_logger.go
+++ compiler/date_logger.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func printFileModificationsDate() {
+func printFileModificationDate() {
 	date := time.Now()
 	actualDate := fmt.Sprintf("\033[33;1m [%d/%02d/%02d %02d:%02d:%02d] \033[0m",
 		date.Year(), date.Month(), date.Day(),
