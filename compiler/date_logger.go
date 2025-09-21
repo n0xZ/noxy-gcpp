e1a388ae13b961db05ac4d396e6e87608d5f74f7
Gon <o.gonzalo@hotmail.com>
2025-09-21 14:35:20 -0300

    feat: add yellow styling on date log

--- compiler/date_logger.go
+++ compiler/date_logger.go
@@ -7,7 +7,7 @@ import (
 
 func printFileModificationsDate() {
 	date := time.Now()
-	actualDate := fmt.Sprintf("[%d/%02d/%02d %02d:%02d:%02d]",
+	actualDate := fmt.Sprintf("\033[33;1m [%d/%02d/%02d %02d:%02d:%02d] \033[0m",
 		date.Year(), date.Month(), date.Day(),
 		date.Hour(), date.Minute(), date.Second())
 	fmt.Print(actualDate)
